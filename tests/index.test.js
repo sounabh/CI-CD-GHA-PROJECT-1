e874473959b8aef6c214039dd5f04a94e9b17e48
sounabh <bagsounabh2003@gmail.com>
2026-06-03 10:08:24 +0530

    first commit

---
+++ tests/index.test.js
@@ -0,0 +1,16 @@
+const { add, subtract } = require('./math');
+
+describe('Math Functions', () => {
+  test('should add two numbers', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test('should subtract two numbers', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
+
+  test('should handle negative numbers', () => {
+    expect(add(-1, -2)).toBe(-3);
+    expect(subtract(-1, -2)).toBe(1);
+  });
+});
